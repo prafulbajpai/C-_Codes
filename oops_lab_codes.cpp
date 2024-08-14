7a4a73dce753db2713e7d309340b099d6b9fbbd1
prafulbajpai <166216635+prafulbajpai@users.noreply.github.com>
2024-08-14 16:42:36 +0530

    Add files via upload

---
+++ oops_lab_codes.cpp
@@ -0,0 +1,94 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	// program number 1 sum of 5 number and their average
+//	int a, b,c;
+//	cout<<"enter two numbers to add/n";
+//	cin>> a>> b;
+//	c= a+b;
+//	cout<<"sum of entered numbers = " << c <<endl;
+//cout<<"average is <<(a+b)/2;
+     
+	 
+	 //program 2 -> hello world 
+//    cout<<"hello world";
+
+
+      //program 3 aschi value 
+//      char a;
+//      cout<<"enter your character";
+//      cin >>a;
+//      cout<<(int)a;
+
+
+    //program number 4 -> quiteint and remainder 
+//    int a, b , q, r ;
+//    cout<<"enter your both number"<<endl;
+//    cin>> a;
+//    cin>>b;
+//    q= a/b;r= a%b ;
+//    cout<<"quotient= "<<q<<" "<< "remainder =" <<r; 
+
+
+      // program number 5 -> simple intrest
+//int p, r,t;
+//cout<<" enter your principle , rate , time "<<endl;
+//cin>>p;cin>>r; cin>>t;
+//int simple_intrest= (p*r*t)/100;
+//cout<<"your simple intrest is =" << simple_intrest;  
+
+
+      //program 6 -> swap two variables 
+//int a, b ,temp;
+//cout <<"enter a "<<endl;
+//cin>>a;
+//cout<< "enter b"<<endl;
+//cin>>b;
+//
+//temp= a;
+//a=b;
+//b=temp;
+//cout<<"value of a after swap="<<a<<endl;
+//cout<<"value of b after swap="<<b<,endl;
+
+
+      // program number 7 -> 
+//int a, b ;
+//cout<<"enter a "<<endl;
+//cin>>a;
+//cout<<"enter b "<< endl;
+//cin>>b;
+//
+//swap(a,b);
+//cout<<"value of a"<<a<<endl;
+//cout<<"value of b"<<b<<endl;
+
+
+// program 8 -> celcius to frehanite
+//int a;
+//cout<<"enter the temp in celcius ";
+//cin>>a;
+//
+//int f =( 9/5) * a + 32;
+//cout<<"temp in f"<<f<<endl;
+ 
+
+// program number 9-> area of a circle
+//int r;
+//cout<<"enter radius "<<endl;
+//cin>>r;
+//cout<<"area of a circle = "<<(22/7) * r * r;
+
+
+// program 10-> days weeks and year 
+// int n_days;
+// cout<<"enter number of days"<<endl;
+// cin>>n_days;
+
+// cout<<"years =" <<n_days / 365 <<endl;
+// cout<<"weeks="<<(n_days % 365) / 7<<endl;
+// cout<<"days =" <<(  n_days % 365) % 7<<endl; 
+// 	return 0;
+// }
+
